8b82d2db288c2689f97153240db7a08acbd3ed8f
iOS-koara <ko.ara@cashwalk.io>
2026-02-15 19:05:54 +0900

    Create a new project and explore the canvas

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by ara on 2/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by ara on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
