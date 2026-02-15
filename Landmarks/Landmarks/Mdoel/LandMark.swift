26a036459dce63426ee5a4b0925b07a19f829214
iOS-koara <ko.ara@cashwalk.io>
2026-02-15 19:38:22 +0900

    Create a landmark model

---
+++ Landmarks/Landmarks/Mdoel/LandMark.swift
@@ -0,0 +1,36 @@
+//
+//  LandMark.swift
+//  Landmarks
+//
+//  Created by ara on 2/15/26.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var parkR: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
