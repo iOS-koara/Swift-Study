8a729e1b0570de9dcf89f81b0084678c63c03346
iOS-koara <ko.ara@cashwalk.io>
2026-02-16 21:33:07 +0900

    Create the list of landmarks

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by ara on 2/16/26.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
