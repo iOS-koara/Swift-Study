606a0cd8740c7f9bc9f765ee88a9130b4e7daefc
iOS-koara <ko.ara@cashwalk.io>
2026-02-17 19:25:10 +0900

    Create a view to represent a UIPageViewController

---
+++ Landmarks/Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,23 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by ara on 2/17/26.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+
+
+    var body: some View {
+        PageViewController(pages: pages)
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+
+#Preview {
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+}
